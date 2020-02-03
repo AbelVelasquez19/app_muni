ec7e164c64b7542f1c89e51286b29436054d19ed
Abel Velasquez <abel.velasquez1997@gmail.com>
2020-02-03 17:24:12 -0500

    diseño de contacto finalizado

--- application/views/gestion/contacto.php
+++ application/views/gestion/contacto.php
@@ -23,7 +23,7 @@
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>asset/template/font-awesome-4.7.0/css/font-awesome.min.css">
     <!-- css para asistencia-->
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>asset/template/css/asistencia.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>asset/template/css/contacto/contacto1.css">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
     <!-- font para reloj-->
     <link href="https://fonts.googleapis.com/css?family=Black+Ops+One&display=swap" rel="stylesheet">
@@ -35,12 +35,88 @@
 <!-- aside en archivo views/admin/layouts/header.php-->
 <main class="app-content">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-5">
+            <div class="tile" style="padding-left:35px;padding-right:35px">
+                <div class="row row-buscar">
+                    <div class="col-md-12 buscar_contribuyente">
+                        <label for="codigo" class="label-control label-text">Buscar Contribuyente</label>
+                        <div class="col-6 input-group mb-2" style="margin-left: -17px;margin-top: -17px;">
+                            <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
+                            <div class="input-group-prepend ">
+                                <div class="input-group-text"><i class="fa fa-search" aria-hidden="true"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 mt-2 buscar_contribuyente">
+                        <div class="form-row">
+                            <div class="col-3">
+                                <label for="codigo" class="label-control label-text">Código</label>
+                                <input type="text" class="form-control text-text" placeholder="Nombre del contribuyente" name="codigo" id="codigo" value="000001">
+                            </div>
+                            <div class="col">
+                                <label for="nombre" class="label-control label-text">Nombre</label>
+                                <input type="text" class="form-control text-text" placeholder="Nombre del contribuyente" name="nombre" id="nombre" value="juan vasquez gregorio">
+                            </div>
+                        </div>
+                        <div class="form-row" style="margin-top:5px;margin-bottom: 5px;">
+                            <div class="col">
+                                <label for="direccion" class="label-control label-text">Dirección</label>
+                                <input type="text" class="form-control text-text" placeholder="direccion del contribuyente" name="direccion" id="direccion">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-7">
             <div class="tile">
-                <h1>MANTENIMIENTO</h1>
+                <div class="row mb-1" style="margin-left:-17px">
+                    <div class="col-3 ">
+                        <button class="btn-nuevo" id="btn_nuevo">NUEVO</button>
+                    </div>
+                </div>
+                <div class="row" style="margin-left:-17px;margin-right:-16px">
+                    <div class="col-12">
+                    <table class="table-personalizado">
+                        <thead >
+                            <tr class="titulo-table text-center">
+                                <th scope="col" style="width:20px">ID</th>
+                                <th scope="col">CELULAR</th>
+                                <th scope="col">TELEFONO</th>
+                                <th scope="col">CORREO</th>
+                                <th scope="col" style="text-align:center;width:30px">OPCIÓN</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td  scope="row"  style="padding:4px;border:2px #A21F12 solid">1</td>
+                                <td  style="padding:4px; border:2px #A21F12 solid">Mark</td>
+                                <td  style="padding:4px; border:2px #A21F12 solid">Otto</td>
+                                <td  style="padding:4px; border:2px #A21F12 solid">@mdo</td>
+                                <td  style="padding:4px; border:2px #A21F12 solid;text-align:center;">
+                                    <button class="btn-editar"><i class="fa fa-pencil-square fa-1x" aria-hidden="true"></i></button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td  scope="row"  style="padding:4px;border:2px #A21F12 solid">1</td>
+                                <td  style="padding:4px; border:2px #A21F12 solid">Mark</td>
+                                <td  style="padding:4px; border:2px #A21F12 solid">Otto</td>
+                                <td  style="padding:4px; border:2px #A21F12 solid">@mdo</td>
+                                <td  style="padding:4px; border:2px #A21F12 solid;text-align:center;">
+                                    <button class="btn-editar"><i class="fa fa-pencil-square fa-1x" aria-hidden="true"></i></button>
+                                </td>
+                            </tr>
+                        </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+        
+
 </main>
 <!-- aside en archivo views/admin/layouts/footer.php-->
 <script src="<?php echo base_url();?>asset/template/js/jquery-3.2.1.min.js"></script>
